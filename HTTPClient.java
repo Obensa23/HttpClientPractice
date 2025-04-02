c20494ba1def3241d96820613114d5b97306b42d
Adrián Obensa <adrianobensa@gmail.com>
2025-04-02 19:32:03 +0200

    Primer comit

---
+++ HTTPClient.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.Socket;
+
+public class HTTPClient {
+    public static void main(String[] args) {
+        String server = "example.com";
+        int port = 80;
+        
+        String request = "GET / HTTP/1.1\r\n"
+                       + "Host: " + server + "\r\n"
+                       + "Connection: close\r\n"
+                       + "\r\n";
+        
+        try (Socket socket = new Socket(server, port);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            
+            out.println(request);
+            
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
